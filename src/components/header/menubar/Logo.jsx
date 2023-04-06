8ec15b940109eeb8f6c2d60005e94edebab5292c
NaashNix <thilakshanaravindu@gmail.com>
2023-04-06 20:41:29 +0530

    First commit

---
+++ src/components/header/menubar/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Logo.module.css';
+
+const Logo = () => {
+   return (
+      <div className={classes.} >
+         <span>NAASHNIX</span>
+      </div>
+   );
+}
+
+export default Logo;
